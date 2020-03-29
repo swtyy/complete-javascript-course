050efe6ca632dca3f1345b40e77cefb156df7de2
sweety kumari <sweety.040993@gmail.com>
2020-03-29 20:37:51 +0530

    Concepts on Function
    Signed-off-by: sweety kumari <sweety.040993@gmail.com>

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -5,7 +5,7 @@ var john = {
     job : 'student'
 };
 console.log(john.name + ' works as a ' + john.job);
-
+1
 var Person = function (name, job, yearOfBirth){
     this.name = name;
     this.job = job;
@@ -28,6 +28,58 @@ console.log(rohit.name + ' works as a ' + rohit.job);
 var subhash = new Person('Subhash', 'Employee', 1988);
 console.log(subhash.name + ' works as a ' + subhash.job);
 subhash.calculateAge(); 
+
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalculator (arr, fn) {
+    var outArray = [];
+    for(var i = 0; i < arr.length; i++){
+        var result = fn(arr[i]);
+        outArray.push(result);
+    }
+return outArray;
+}
+
+var calAge2020 = function( year ){
+    return 2020 - year;
+}
+
+var calAge2000 = function( year ){
+    return 2000 - year;
+}
+
+var result = arrayCalculator (years, calAge2000);
+
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
